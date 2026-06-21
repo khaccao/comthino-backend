231f8300ce81b03d28315dd72c72f5f1a2f9f870
khaccao <nguyenkhaccao1@gmail.com>
2026-06-21 22:24:04 +0700

    Make SSL domains configurable

--- scripts/request-ssl.sh
+++ scripts/request-ssl.sh
@@ -3,14 +3,19 @@ set -euo pipefail
 
 EMAIL="${SSL_EMAIL:-admin@comthino.com}"
 ACI_ROOT="${ACI_ROOT:-/root/apps/AciPlatform}"
+SSL_DOMAINS="${SSL_DOMAINS:-comthino.com}"
+
+DOMAIN_ARGS=()
+for domain in $SSL_DOMAINS; do
+  DOMAIN_ARGS+=("-d" "$domain")
+done
 
 docker run --rm --name certbot-comthino \
   -v "$ACI_ROOT/data/certbot/conf:/etc/letsencrypt" \
   -v "$ACI_ROOT/data/certbot/www:/var/www/certbot" \
   certbot/certbot certonly --webroot \
   -w /var/www/certbot \
-  -d comthino.com \
-  -d www.comthino.com \
+  "${DOMAIN_ARGS[@]}" \
   --email "$EMAIL" \
   --agree-tos \
   --no-eff-email
